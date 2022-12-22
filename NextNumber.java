f8748b874e787eded6a322b40c09e95809c19bf4
Ayush Vaidya <83414737+Ayush-Vaidya@users.noreply.github.com>
2022-12-22 14:14:59 +0530

    Add files via upload

---
+++ NextNumber.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+public class NextNumber {
+    public static void main(String args[]) {
+        Scanner s = new Scanner(System.in);
+        boolean d;
+
+        d = s.hasNextInt();
+        System.out.println(d);
+
+    }
+
+}
---
+++ Printing.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+
+public class Printing {
+
+    public static void main(String args[]) {
+        String str1 = "ayush.vaidya2020@vitbhopal.ac.in";
+        System.out.println(str1.matches(".*@vitbhopal.ac.in"));
+
+    }
+}
