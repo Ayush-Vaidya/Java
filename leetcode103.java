d9aae66d5d2eb19bfc6a4c3d3479e838347a2d34
Ayush Vaidya <83414737+Ayush-Vaidya@users.noreply.github.com>
2023-02-19 15:23:31 +0530

    Create leetcode103.java

---
+++ leetcode103.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        final List<List<Integer>> ans = new ArrayList<>();
+        // sanity check
+        if (root == null)
+            return ans;
+
+        final Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(root);
+        int level = 0;
+
+        while (!queue.isEmpty()) {
+            final int sizeQ = queue.size();
+            final List<Integer> intermediate = new ArrayList<>();
+
+            for (int i = 0; i < sizeQ; ++i) {
+                final TreeNode cur = queue.removeFirst();
+                intermediate.add(cur.val);
+
+                if (cur.left != null)
+                    queue.add(cur.left);
+                if (cur.right != null)
+                    queue.add(cur.right);
+            }
+            if (level % 2 != 0) Collections.reverse(intermediate);
+            ++level;
+            ans.add(new ArrayList<>(intermediate));
+        }
+
+        return ans;
+    }
+}
