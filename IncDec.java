f1387f5527d7990774a9f33c28afb5971b9e33b8
Ayush Vaidya <83414737+Ayush-Vaidya@users.noreply.github.com>
2023-03-20 21:35:52 +0530

    Add files via upload

---
+++ IncDec.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+class IncDec {
+    public static void main(String args[]) {
+        int x = 5, y = 4, z;
+        z = 2 * x++ + 3 * ++x;
+
+        System.out.println(z);
+    }
+}
