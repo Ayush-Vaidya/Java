078b1e3cf681064c720e23cbcc7bc4e4c6560d29
Ayush Vaidya <83414737+Ayush-Vaidya@users.noreply.github.com>
2023-03-29 18:28:35 +0530

    Create leetcode1388.java

---
+++ leetcode1388.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+        int len = satisfaction.length;
+        if(satisfaction[len-1]<=0) return 0;
+        int max = satisfaction[len-1];
+        int base = max;
+        for(int i=len-2; i>=0; i--){
+            int next = satisfaction[i]+max+base;
+            if(next >= max){
+                max = next;
+                base+=satisfaction[i];
+            }else
+                return max;
+        }
+        return max;
+    }
+}
