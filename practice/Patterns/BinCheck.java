88e1c6e358aa08c97f887890e927735525caf912
Ayush-Vaidya <vaidya2001ayush@gmail.com>
2023-07-31 17:12:14 +0530

    data

---
+++ practice/Patterns/BinCheck.java
@@ -0,0 +1,13 @@
+package javap;
+import java.lang.*;
+import java.security.PublicKey;
+import java.util.*;
+
+public class BinCheck {
+    public static void main(String[] arg) {
+        int b = 10101101;
+        String str = String.valueOf(b);
+        System.out.println(str.matches("[01]+"));
+
+    }
+}
