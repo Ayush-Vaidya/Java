8a1e1ff021300e4823e3d8731e205f2f1a9e6d11
Ayush-Vaidya <vaidya2001ayush@gmail.com>
2023-08-19 17:12:14 +0530

    data

---
+++ practice/Patterns/OOPS.java
@@ -0,0 +1,113 @@
+package OOPS;
+public class OOPS {
+    public static void main(String args[]) {
+ 
+ 
+
+
+    }
+}
+
+
+
+// class Student{
+//     String name;
+//     int roll;
+//     String password;
+//     int marks[];
+
+//     Student(Student s1){
+//         marks = new int[3];
+//         this.name= s1.name;
+//         this.roll= s1.roll;
+//         this.marks=s1.marks; 
+//     }
+//     Student(){
+//         marks = new int[3];
+//         System.out.println("Constructor is called");
+//     }
+//     Student( String name){
+//         marks = new int[3];
+//         this.name=name;
+//     }
+//     Student(int roll){
+//         marks = new int[3];
+//         this.roll=roll;
+//     }
+// }
+
+// class Animal{
+//     String color;
+//     void eat(){
+//         System.out.println("eats anything");
+//     }
+//     void breathe(){
+//         System.out.println("breathes");
+//     }
+// }
+// class deer extends Animal{
+//     void eat(){
+//         System.out.println("eats grass");
+//     }
+// }
+// class mammal extends Animal{
+//     void walks(){
+//         System.out.println("walks");
+//     }
+// }
+// class fish extends Animal{
+//     void swims(){
+//         System.out.println("swims");
+//     }
+// }
+// class bird extends Animal{
+//     void fly(){
+//         System.out.println("fly");
+//     }
+// }
+
+// class dog extends mammal{
+//     String breed;
+// }
+
+// //derived class
+// // class fish extends Animal{
+// //     int fins;
+// //     void swim(){
+// //         System.out.println("swims");
+// //     }
+// // }
+
+
+// class Pen {
+//     //properties + fuctions
+//     private String color;
+//     private int tip;
+//     String getColor(){
+//         return this.color;
+//     }
+//     int getTip(){
+//         return this.tip;
+//     }
+//     void setColor(String newColor){
+//         color = newColor;
+//     }
+//     void setTip(int newTip){
+//         tip = newTip;
+//     }
+
+
+
+// }
+
+// class calculator{
+//     int sum(int a, int b){
+//         return a+b;
+//     }
+//     float sum(float a, float b){
+//         return a+b;
+//     }
+//     float sum( int a, int b, int c){
+//         return a+b+c;
+//     }
+// }
